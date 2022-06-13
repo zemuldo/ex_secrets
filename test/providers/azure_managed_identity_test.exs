924acdcefff8fedaffbaac738aadb0d74c8d4107
Zemuldo <danstan.otieno@gmail.com>
2022-06-13 23:13:42 +0300

    clean azure managed identity tests'

--- test/providers/azure_managed_identity_test.exs
+++ test/providers/azure_managed_identity_test.exs
@@ -24,78 +24,33 @@ defmodule ExSecrets.Providers.AzureKeyManagedIdentityTest do
 
   test "Get Secret Azure Managed Identity" do
     AzureManagedIdentityHTTPAdapterMock
-    |> expect(
-      :get,
-      fn url, _ ->
-        case url do
-          "http://169.254.169.254" <> _ ->
-            {:ok,
-             %HTTPoison.Response{
-               body:
-                 "{\"token_type\":\"Bearer\",\"expires_in\":3599,\"ext_expires_in\":3599,\"access_token\":\"dummy_access_token\"}",
-               request_url:
-                 "https://login.microsoftonline.com/9fb676fa-7f2c-4b77-9de7-9b9d1b56db3f/oauth2/v2.0/token",
-               status_code: 200
-             }}
-
-          "https://key-vault-name.vault.azure.ne" <> _ ->
-            {:ok,
-             %HTTPoison.Response{
-               body: "{\"value\":\"DOTXYZHASH\"}",
-               status_code: 200
-             }}
-        end
-      end
-    )
-    |> expect(
-      :get,
-      fn url, _ ->
-        case url do
-          "http://169.254.169.254" <> _ ->
-            {:ok,
-             %HTTPoison.Response{
-               body:
-                 "{\"token_type\":\"Bearer\",\"expires_in\":3599,\"ext_expires_in\":3599,\"access_token\":\"dummy_access_token\"}",
-               request_url:
-                 "https://login.microsoftonline.com/9fb676fa-7f2c-4b77-9de7-9b9d1b56db3f/oauth2/v2.0/token",
-               status_code: 200
-             }}
-
-          "https://key-vault-name.vault.azure.net" <> _ ->
-            {:ok,
-             %HTTPoison.Response{
-               body: "{\"value\":\"DOTXYZHASH\"}",
-               status_code: 200
-             }}
-        end
-      end
-    )
-    |> expect(
-      :get,
-      fn url, _ ->
-        case url do
-          "http://169.254.169.254" <> _ ->
-            {:ok,
-             %HTTPoison.Response{
-               body:
-                 "{\"token_type\":\"Bearer\",\"expires_in\":3599,\"ext_expires_in\":3599,\"access_token\":\"dummy_access_token\"}",
-               request_url:
-                 "https://login.microsoftonline.com/9fb676fa-7f2c-4b77-9de7-9b9d1b56db3f/oauth2/v2.0/token",
-               status_code: 200
-             }}
-
-          "https://key-vault-name.vault.azure.net" <> _ ->
-            {:ok,
-             %HTTPoison.Response{
-               body: "{\"value\":\"DOTXYZHASH\"}",
-               status_code: 200
-             }}
-        end
-      end
-    )
+    |> expect(:get, &api_mock/2)
+    |> expect(:get, &api_mock/2)
+    |> expect(:get, &api_mock/2)
 
     {:ok, _} = GenServer.start(AzureManagedIdentity, [], name: AzureManagedIdentity)
 
     assert ExSecrets.get("ABC", :azure_managed_identity) == "DOTXYZHASH"
+  end
+
+  defp api_mock(url, _) do
+    case url do
+      "http://169.254.169.254" <> _ ->
+        {:ok,
+         %HTTPoison.Response{
+           body:
+             "{\"token_type\":\"Bearer\",\"expires_in\":3599,\"ext_expires_in\":3599,\"access_token\":\"dummy_access_token\"}",
+           request_url:
+             "https://login.microsoftonline.com/tenant-id/oauth2/v2.0/token",
+           status_code: 200
+         }}
+
+      "https://key-vault-name.vault.azure.net" <> _ ->
+        {:ok,
+         %HTTPoison.Response{
+           body: "{\"value\":\"DOTXYZHASH\"}",
+           status_code: 200
+         }}
+    end
   end
 end
