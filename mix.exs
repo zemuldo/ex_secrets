1818128f23c2291e3df361c4d1a8878d3aeb966f
Zemuldo <danstan.otieno@gmail.com>
2022-06-14 00:01:51 +0300

    hex v0.1.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,10 @@ defmodule ExSecrets.MixProject do
       app: :ex_secrets,
       version: "0.1.0",
       elixir: "~> 1.13",
-      start_permanent: Mix.env() == :prod,
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -25,7 +28,21 @@ defmodule ExSecrets.MixProject do
       {:httpoison, "~> 1.8"},
       {:poison, "~> 5.0"},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:mox, "~> 1.0", only: :test}
+      {:mox, "~> 1.0", only: :test},
+       {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Provider secrets to your app from .env, Azure KeyVault, Azure Managed Identity etc."
+  end
+
+  defp package() do
+    [
+      name: "ex_secrets",
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE* CHANGELOG*),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/zemuldo/ex_secrets"}
     ]
   end
 end
