f59e913bb57ba4041104daf29dc4318ff2532133
Zemuldo <danstan.otieno@gmail.com>
2022-07-13 15:56:56 +0300

    remove google and aws providers

--- lib/utils/resolver.ex
+++ lib/utils/resolver.ex
@@ -3,7 +3,5 @@ defmodule ExSecrets.Utils.Resolver do
   def call(:dot_env), do: ExSecrets.Providers.DotEnv
   def call(:azure_managed_identity), do: ExSecrets.Providers.AzureManagedIdentity
   def call(:azure_key_vault), do: ExSecrets.Providers.AzureKeyVault
-  def call(:google_secret_manager), do: ExSecrets.Providers.GoogleSecretManager
-  def call(:aws_secrets_manage), do: ExSecrets.Providers.AwsSecretsManager
   def call(_), do: {:error, "Unknown secret provider"}
 end
