3aab652630c6c33ca99b1305fd298d663238e39f
Zemuldo <danstan.otieno@gmail.com>
2023-10-24 02:06:49 +0300

    adds certificate auth for azure kv client

--- lib/providers/azure_key_vault.ex
+++ lib/providers/azure_key_vault.ex
@@ -3,8 +3,80 @@ defmodule ExSecrets.Providers.AzureKeyVault do
 
   alias ExSecrets.Utils.Config
 
+  @scope "https://vault.azure.net/.default"
+
   @moduledoc """
-  Azure Key Vault provider provides secrets from an Azure Key Vault through a rest API.
+  Azure Key Vault provider provides secrets from an [Azure Key Vault](https://learn.microsoft.com/en-us/azure/key-vault/general/overview) through a rest API.
+
+  ## Configuration
+
+  You can configure this provider as shown below. Either `client_secret` or `client_cert_path` is required to implement authentication.
+  Client `client_certificate` is recommended for security. If you provide both, `client_certificate` is used. All other config options are mandatory.
+  ```
+  Azure KeyVault configuration:
+      config :ex_secrets, :providers, %{
+        azure_key_vault: %{
+          tenant_id: "tenant-id",
+          client_id: "client-id",
+          client_secret: "client-secret",
+          client_certificate: "/path/cert.key",
+          client_certificate_x5t: "x5t",
+          key_vault_name: "key-vault-name"
+        }
+  ```
+
+  See notes below on setting up authentication.
+
+  ## Authentication
+  The service pricipal being used must have a role that can access the secrets eg `Key Vault Secrets User`.
+
+  ### Secret Authentication
+  This is prety straing forward. See the stps below from Being Chat
+  Here is a step-by-step guide to generate a secret for an app on Azure AD App Registration Secrets page:
+
+    1. Sign in to the Azure portal.
+    2. Navigate to the Azure Active Directory > App registrations > Owned applications.
+    3. Select your application.
+    4. Click on Certificates & secrets > Client secrets > New client secret.
+    5. Type a description and an expiration for the client’s secret.
+    6. Click Add.
+
+    [For more information on generating client secrets in Azure AD, please refer to 12](https://o365info.com/create-unlimited-client-secret/)
+
+  ### Certificate Authentication
+  MacOS and many Linux distributions come with pre-compiled OpenSSL packages. You can run the following command directly from a shell to confirm if OpenSSL has already been installed.
+
+  Create a Certificate Signing Request (.csr file) and generate a private key (.key file) using the following command:
+
+  ```
+  openssl req -newkey rsa:4096 -nodes -keyout mycert.key -batch -out mycert.csr
+  ```
+
+  This command will generate a simple CSR and download a 4096-bit private key in your current directory for self-signature.
+
+  Next, self-sign the certificate using the private key that was just generated:
+  ```
+  openssl x509 -key mycert.key -in mycert.csr -req -days 3650 -out mycert.crt
+  ```
+
+  You may replace validity period 3650 with any number of days you wish. Just be aware that once the period of validity has expired, you will need to replace the certificate with a new one.
+
+  Upload the .crt file to Azure portal using the Being Chat steps below.
+
+  Here is a step-by-step guide to upload a certificate for an app on Azure AD App Registration Secrets page:
+
+    1. Sign in to the Azure portal.
+    2. Navigate to the Azure Active Directory > App registrations > Owned applications.
+    3. Select your application.
+    4. Click on Certificates & secrets > Certificates > Upload certificate.
+    4. Browse to the `.crt` certificate file saved on your local machine and select it.
+    5. Type a description for the certificate.
+    7. Click Add.
+
+  Finally generate the `x5t` JWT header required by Entra using the command below. See https://learn.microsoft.com/en-us/entra/identity-platform/certificate-credentials#assertion-format.
+  ```
+  openssl x509 -in mycert.crt -fingerprint -noout) | sed 's/SHA1 Fingerprint=//g' | sed 's/://g' | xxd -r -ps | base64
+  ```
   """
 
   @headers %{"Content-Type" => "application/x-www-form-urlencoded"}
@@ -65,7 +137,8 @@ defmodule ExSecrets.Providers.AzureKeyVault do
   end
 
   defp get_secret(name, state, _) do
-    with {:ok, %{"access_token" => access_token} = new_state} <- get_access_token(),
+    with {:ok, %{"access_token" => access_token} = new_state} <-
+           get_access_token(),
          {:ok, value} <- get_secret_call(name, access_token) do
       {:ok, value, state |> Map.merge(new_state) |> Map.put("issued_at", get_current_epoch())}
     else
@@ -91,28 +164,78 @@ defmodule ExSecrets.Providers.AzureKeyVault do
 
   defp get_access_token() do
     client = http_adpater()
-    client_id = Config.provider_config_value(:azure_key_vault, :client_id)
     client_secret = Config.provider_config_value(:azure_key_vault, :client_secret)
+    client_certificate = Config.provider_config_value(:azure_key_vault, :client_certificate)
     tenant_id = Config.provider_config_value(:azure_key_vault, :tenant_id)
-    scope = "https://vault.azure.net/.default"
 
-    req_body =
-      URI.encode_query(%{
-        "client_id" => client_id,
-        "client_secret" => client_secret,
-        "scope" => scope,
-        "grant_type" => "client_credentials"
-      })
-
-    with {:ok, %{body: body, status_code: 200}} <-
+    with req_body when is_binary(req_body) <-
+           build_claims_body(%{"secret" => client_secret, "cert" => client_certificate}),
+         {:ok, %{body: body, status_code: 200}} <-
            tenant_id
            |> token_uri()
            |> client.post(req_body, @headers),
          {:ok, data} <- Poison.decode(body) do
       {:ok, data |> Map.put("issued_at", get_current_epoch())}
     else
+      {:error, term} ->
+        {:error, term}
+
       _ ->
         {:error, "Failed to get access token"}
+    end
+  end
+
+  defp build_claims_body(%{"cert" => cert}) when is_binary(cert) do
+    client_id = Config.provider_config_value(:azure_key_vault, :client_id)
+
+    URI.encode_query(%{
+      "client_id" => client_id,
+      "scope" => @scope,
+      "grant_type" => "client_credentials",
+      "client_assertion_type" => "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+      "client_assertion" => jwt()
+    })
+  end
+
+  defp build_claims_body(%{"secret" => secret}) when is_binary(secret) do
+    client_id = Config.provider_config_value(:azure_key_vault, :client_id)
+
+    URI.encode_query(%{
+      "client_id" => client_id,
+      "client_secret" => secret,
+      "scope" => @scope,
+      "grant_type" => "client_credentials"
+    })
+  end
+
+  defp build_claims_body(_), do: {:error, :no_auth}
+
+  defp jwt() do
+    client_id = Config.provider_config_value(:azure_key_vault, :client_id)
+    client_certificate = Config.provider_config_value(:azure_key_vault, :client_certificate)
+
+    client_certificate_x5t =
+      Config.provider_config_value(:azure_key_vault, :client_certificate_x5t)
+
+    tenant_id = Config.provider_config_value(:azure_key_vault, :tenant_id)
+    t = DateTime.to_unix(DateTime.utc_now())
+
+    signer =
+      Joken.Signer.create("RS256", %{"pem" => File.read!(client_certificate)}, %{
+        "x5t" => client_certificate_x5t
+      })
+
+    claims = %{
+      "iss" => client_id,
+      "sub" => client_id,
+      "aud" => "https://login.microsoftonline.com/#{tenant_id}/oauth2/v2.0/token",
+      "exp" => t + 1200,
+      "iat" => t
+    }
+
+    case Joken.encode_and_sign(claims, signer) do
+      {:ok, jwt, _} -> jwt
+      _ -> ""
     end
   end
 
